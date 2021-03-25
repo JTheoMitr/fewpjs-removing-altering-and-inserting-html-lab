3451a2d20abafd55fd33e1ed1662ef923b98ac6d
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2021-03-25 01:13:41 -0500

    commit tree 9908e9dba5a52794415bccdac6fb37f7c02444f8

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+main.remove()
+
+let newHeader = document.createElement('h1')
+
+newHeader.id = 'victory'
+
+newHeader.innerHTML = 'Amaya is the champion and the best dog in the world'
